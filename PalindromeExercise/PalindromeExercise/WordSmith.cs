b19bd32e9b39380449c2f7305cc0dd623d08a124
James <jamesmcdaniel33@hotmail.com>
2023-06-03 13:03:28 -0500

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public bool IsAPalindrome(string word)
+		{
+			string reversed = String.Empty;
+			for(int i = word.Length -1; i >= 0; i--)
+			{
+				reversed += word[i];
+			}
+			if(word.ToLower() == reversed.ToLower())
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,24 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("smart", false)]
+        [InlineData("Otto", true)]
+        [InlineData("rocket", false)]
+        [InlineData("eye", true)]
+        public void Test1(string word, bool expected)
         {
+            WordSmith wordsmith = new WordSmith();
 
+            var actual = wordsmith.IsAPalindrome(word);
+
+            Assert.Equal(actual, expected);
         }
     }
 }
